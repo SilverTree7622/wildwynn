8a300e9a818ae1e78dadc822a27d269b3dcf447a
FluxHunter <40912698+SilverTree7622@users.noreply.github.com>
2024-05-16 16:38:14 +0900

    Updates

--- layouts/sporttab.vue
+++
@@ -1,9 +0,0 @@
-<template>
-  <div
-    class="overflow-hidden w-[100vw] h-[100vh] bg-[#181818] selection:bg-black flex flex-col justify-center items-center text-white"
-  >
-    <slot></slot>
-  </div>
-</template>
-
-<script setup lang="ts"></script>
--- pages/index.vue
+++
@@ -1,9 +0,0 @@
-<template>
-	<div style="background: #001226;"></div>
-</template>
-
-<script lang="ts" setup>
-onBeforeMount(() => {
-	navigateTo('/FootBall');
-});
-</script>
