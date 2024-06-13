2cd3f11c9d23e15d6a76d7bebdaf25bc570aa41a
FluxHunter <40912698+SilverTree7622@users.noreply.github.com>
2024-06-14 00:19:19 +0900

    [fix] websocket issue

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -17,14 +17,15 @@ export default defineNuxtConfig({
 	modules: ['@pinia/nuxt', '@nuxt/ui', "@nuxtjs/i18n", 'nuxt-socket-io', ],
 
 	// https://nuxt.com/modules/socket-io
-	io: {
-		// module options
-		sockets: [{
-			name: 'main',
-			url: 'http://localhost:3000'
-		}]
-	},
+	// io: {
+	// 	// module options
+	// 	sockets: [{
+	// 		name: 'main',
+	// 		url: 'http://localhost:3000'
+	// 	}]
+	// },
 
+	// https://nuxt.com/modules/i18n
 	i18n: {
 		strategy: 'prefix_except_default',
 		defaultLocale: 'en',
